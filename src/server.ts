47bce8113ff156ce87c35997beaf56d60761605c
AoWangPhilly <aw3338@drexel.edu>
2023-02-25 01:22:09 -0500

    connect to mongodb

--- src/server.ts
+++ src/server.ts
@@ -5,18 +5,32 @@ import cors from "cors";
 import axios from "axios";
 import * as fs from "fs";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { config } from "./config/config.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config({ path: path.join(__dirname, "../.env")});
+dotenv.config({ path: path.join(__dirname, "../.env") });
 const API_KEY = process.env.API_KEY;
 // console.log(API_KEY);
 
 let app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
+/**
+ * Connect to Mongoose
+ */
+mongoose.set("strictQuery", true);
+mongoose
+    .connect(config.mongo.url, { retryWrites: true, w: "majority" })
+    .then(() => {
+        console.log("connected!");
+    })
+    .catch((error) => {
+        console.log(error);
+    });
 
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
     cors({
@@ -25,7 +39,6 @@ app.use(
     })
 );
 app.use(express.json());
-
 
 app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
