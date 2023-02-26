31c14eb4ee436e9c6e999029dfe051bc36934aaf
Abe Jeyapratap <abejeyapratap@gmail.com>
2023-02-25 20:41:08 -0500

    fixed react start port and mongo username

--- backend/server.ts
+++ backend/server.ts
@@ -11,24 +11,24 @@ import { userProfileRouter } from "./routes/UserProfile.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-dotenv.config({ path: path.join(__dirname, "../.env") });
 const API_KEY = process.env.API_KEY;
+
 // console.log(API_KEY);
+dotenv.config({ path: path.join(__dirname, "../.env") });
 
 let app = express();
 
 /**
- * Connect to Mongoose
+ * Connect to MongoDB
  */
 mongoose.set("strictQuery", true);
 mongoose
     .connect(config.mongo.url, { retryWrites: true, w: "majority" })
     .then(() => {
-        console.log("connected!");
+        console.log("Connected to DB");
     })
     .catch((error) => {
-        console.log(error);
+        console.log("DB connection failed");
     });
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -42,7 +42,7 @@ app.use(
 app.use(express.json());
 
 /**
- * Routers
+ * Routes
  */
 app.use("/api/user/profiles", userProfileRouter);
 
