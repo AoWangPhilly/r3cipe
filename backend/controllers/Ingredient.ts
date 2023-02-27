23bd92cecb2bfa27d98c6bf309a2e5432bb991a8
AoWangPhilly <aw3338@drexel.edu>
2023-02-27 16:28:15 -0500

    add controllers for ingredients

---
+++ backend/controllers/Ingredient.ts
@@ -0,0 +1,91 @@
+import { Request, Response } from "express";
+import IngredientModel, { IIngredient } from "../models/Ingredient.js";
+
+// Create a new ingredient
+export const createIngredient = async (req: Request, res: Response) => {
+    const { id, name } = req.body;
+
+    try {
+        const ingredient: IIngredient = await IngredientModel.create({
+            id,
+            name,
+        });
+        return res.status(201).json({ ingredient });
+    } catch (err) {
+        return res.status(500).json({ error: "Error creating ingredient" });
+    }
+};
+
+// Get all ingredients
+export const getIngredients = async (req: Request, res: Response) => {
+    try {
+        const ingredients: IIngredient[] = await IngredientModel.find();
+        return res.status(200).json({ ingredients });
+    } catch (err) {
+        return res.status(500).json({ error: "Error getting ingredients" });
+    }
+};
+
+// Get a specific ingredient by id
+export const getIngredientById = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+        const ingredient: IIngredient | null = await IngredientModel.findById(
+            id
+        );
+        if (!ingredient) {
+            return res.status(404).json({ error: "Ingredient not found" });
+        }
+        return res.status(200).json({ ingredient });
+    } catch (err) {
+        return res.status(500).json({ error: "Error getting ingredient" });
+    }
+};
+
+// Update an ingredient by id
+export const updateIngredient = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { name } = req.body;
+
+    try {
+        const ingredient: IIngredient | null =
+            await IngredientModel.findByIdAndUpdate(
+                id,
+                { name },
+                { new: true }
+            );
+        if (!ingredient) {
+            return res.status(404).json({ error: "Ingredient not found" });
+        }
+        return res.status(200).json({ ingredient });
+    } catch (err) {
+        return res.status(500).json({ error: "Error updating ingredient" });
+    }
+};
+
+// Delete an ingredient by id
+export const deleteIngredient = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+        const ingredient: IIngredient | null =
+            await IngredientModel.findByIdAndDelete(id);
+        if (!ingredient) {
+            return res.status(404).json({ error: "Ingredient not found" });
+        }
+        return res
+            .status(200)
+            .json({ message: "Ingredient deleted successfully" });
+    } catch (err) {
+        return res.status(500).json({ error: "Error deleting ingredient" });
+    }
+};
+
+export default {
+    createIngredient,
+    getIngredients,
+    getIngredientById,
+    updateIngredient,
+    deleteIngredient,
+};
