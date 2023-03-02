4055f781349ab21c542000d68eaa602bdc049743
Abe Jeyapratap <abejeyapratap@gmail.com>
2023-03-01 21:30:42 -0500

    logout endpoint and logic

--- backend/routes/auth.ts
+++ backend/routes/auth.ts
@@ -4,6 +4,6 @@ import controller from "../controllers/auth.js";
 const router = Router();
 
 router.post("/login", controller.login);
-// router.post("/logout", controller.logout);
+router.post("/logout", controller.logout);
 
 export default router;
