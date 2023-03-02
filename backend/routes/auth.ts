dec9f0b9533e798bd68b343eb0b6958b3d5d037e
Abe Jeyapratap <abejeyapratap@gmail.com>
2023-03-01 21:05:35 -0500

    modularize login logic

--- backend/routes/auth.ts
+++ backend/routes/auth.ts
@@ -4,5 +4,6 @@ import controller from "../controllers/auth.js";
 const router = Router();
 
 router.post("/login", controller.login);
+// router.post("/logout", controller.logout);
 
 export default router;
