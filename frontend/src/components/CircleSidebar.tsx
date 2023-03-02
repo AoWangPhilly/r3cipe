6f74dfc7d0f66e6e135526b836d85574ce6c4921
Daniel Erbynn <dne28@drexel.edu>
2023-03-01 19:42:59 -0500

    add circle signup, join, and routes

---
+++ frontend/src/components/CircleSidebar.tsx
@@ -0,0 +1,118 @@
+import { Box, Button, TextField } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const CircleSidebar = () => {
+    type FormState = {
+        Code: string;
+    };
+    const [formState, setFormState] = useState<FormState>({
+        Code: "",
+    });
+
+    const handleInputChange = (
+        e: React.ChangeEvent<HTMLInputElement>
+    ): void => {
+        const { name, value } = e.target;
+        setFormState((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+        e.preventDefault();
+        console.log("Join Circle for Code: " + formState.Code);
+        // Get code from form and submit to backend
+        //  if code is invalid, display error message
+        // axios.post("/api/circle/join", { Code: formState.Code }).then((res) => {
+        //     console.log(res);
+        //     if (res.data.success) {
+        //         //  if code is valid, redirect to selected circle page
+        //     } else {
+        //         // error message
+        //     }
+        // });
+    };
+
+    return (
+        <Box
+            sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                width: "30%",
+                height: "100%",
+                backgroundColor: "transparent",
+                right: 0,
+                position: "fixed",
+                top: "65px", // add top to position the sidebar below the navbar
+            }}
+        >
+            <Typography
+                variant="h6"
+                sx={{
+                    fontWeight: 700,
+                    letterSpacing: ".3rem",
+                    color: "inherit",
+                    textDecoration: "none",
+                }}
+            >
+                Join with Code
+            </Typography>
+            <form onSubmit={handleSubmit}>
+                <TextField
+                    autoComplete="Code"
+                    name="Code"
+                    variant="outlined"
+                    required
+                    fullWidth
+                    id="Code"
+                    label="Code"
+                    value={formState.Code}
+                    autoFocus
+                    onChange={handleInputChange}
+                />
+                <Button
+                    type="submit"
+                    variant="contained"
+                    sx={{
+                        width: "100%",
+                        my: 2,
+                        background:
+                            "linear-gradient(45deg, #2196F3 40%, #21CBF3 80%)",
+                    }}
+                >
+                    Join
+                </Button>
+            </form>
+            <Typography
+                variant="h6"
+                sx={{
+                    fontWeight: 700,
+                    letterSpacing: ".3rem",
+                    color: "inherit",
+                    textDecoration: "none",
+                }}
+            >
+                or
+            </Typography>
+            <Link to="/circlesignup" style={{ textDecoration: "none" }}>
+                <Button
+                    variant="contained"
+                    sx={{
+                        width: "100%",
+                        my: 2,
+                        background:
+                            "linear-gradient(45deg, #2196F3 40%, #21CBF3 80%)",
+                    }}
+                >
+                    Create
+                </Button>
+            </Link>
+        </Box>
+    );
+};
+
+export default CircleSidebar;
--- frontend/src/components/GroupCard.tsx
+++ frontend/src/components/GroupCard.tsx
@@ -1,35 +1,53 @@
 import { FC } from "react";
 import { Card, CardHeader, Avatar, Grid } from "@mui/material";
+import { Link } from "react-router-dom";
 
 interface GroupCardProps {
     title: string;
     subtitle: string;
     avatars: { alt: string; src: string }[];
+    code: string;
 }
-const GroupCard: FC<GroupCardProps> = ({ title, subtitle, avatars }) => {
+
+const GroupCard: FC<GroupCardProps> = ({ title, subtitle, avatars, code }) => {
     return (
-        <Card>
-            <CardHeader
-                title={title}
-                subheader={subtitle}
-                avatar={
-                    <Grid container spacing={2}>
-                        {avatars.map((avatar, index) => (
-                            <Grid item key={index}>
-                                <Avatar
-                                    alt={avatar.alt}
-                                    src={avatar.src}
-                                    sx={{
-                                        background:
-                                            "linear-gradient(45deg, #2196F3 40%, #21CBF3 80%)",
-                                    }}
-                                ></Avatar>
-                            </Grid>
-                        ))}
-                    </Grid>
-                }
-            />
-        </Card>
+        <Link to={`/circle/${code}`} style={{ textDecoration: "none" }}>
+            <Card
+                sx={{
+                    width: "70%",
+                    mb: 2,
+                    transition: "all 0.2s ease-in-out",
+                    "&:hover": {
+                        backgroundColor: "#grey",
+                        boxShadow: "0 0 5px rgba(0, 0, 0, 0.3)",
+                    },
+                }}
+            >
+                <CardHeader
+                    code={code}
+                    title={title}
+                    subheader={subtitle}
+                    avatar={
+                        <Grid container spacing={2}>
+                            {avatars.map((avatar, index) => (
+                                <Grid item key={index}>
+                                    <Avatar
+                                        alt={avatar.alt}
+                                        src={avatar.src}
+                                        sx={{
+                                            width: 100,
+                                            height: 100,
+                                            background:
+                                                "linear-gradient(45deg, #2196F3 40%, #21CBF3 80%)",
+                                        }}
+                                    ></Avatar>
+                                </Grid>
+                            ))}
+                        </Grid>
+                    }
+                />
+            </Card>
+        </Link>
     );
 };
 
