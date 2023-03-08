597f382ed2c1a1e5cf9f89d4a7c0353886724d52
Abe Jeyapratap <abejeyapratap@gmail.com>
2023-03-08 00:16:11 -0500

    modularize session storage code

--- backend/routes/auth.ts
+++ backend/routes/auth.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import controller from "../controllers/auth.js";
+import { authorize } from "../middleware/checkAuth.js";
 
 const router = Router();
 
@@ -7,5 +8,9 @@ router.post("/login", controller.login);
 router.post("/logout", controller.logout);
 // router.post("/signup", controller.signup);
 router.get("/checkLogin", controller.checkLogin);
+
+router.get("/dummy", authorize, (req, res) => {
+    res.json({ message: "classified" });
+})
 
 export default router;
