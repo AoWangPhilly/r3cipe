bfe6e04a5a7868ee7972354912cefe4575a94725
AoWangPhilly <aw3338@drexel.edu>
2023-03-08 21:09:25 -0500

    create inventory on signup

--- backend/controllers/Inventory.ts
+++ backend/controllers/Inventory.ts
@@ -2,29 +2,6 @@ import Inventory from "../models/Inventory.js";
 import { NextFunction, Request, Response } from "express";
 import { getTokenStorage } from "../helpers/tokenStorage.js";
 
-const createInventory = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-) => {
-    try {
-        const { token } = req.cookies;
-        const tokenStorage = getTokenStorage();
-        if (!tokenStorage[token]) {
-            throw new Error("Invalid token");
-        }
-
-        const inventory = await Inventory.create({
-            userId: tokenStorage[token].id,
-        });
-        inventory.save();
-        // Send response
-        res.status(201).json(inventory);
-    } catch (error) {
-        next(error);
-    }
-};
-
 const updatePantry = async (
     req: Request,
     res: Response,
@@ -71,8 +48,8 @@ const getPantry = async (req: Request, res: Response, next: NextFunction) => {
         return res.status(500).json({ message: "Internal server error" });
     }
 };
+
 export default {
-    createInventory,
     updatePantry,
     getPantry,
 };
