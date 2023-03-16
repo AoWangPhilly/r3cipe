3bb9b0f88d0a40dd5b43042dedd379852a225073
Abe Jeyapratap <abejeyapratap@gmail.com>
2023-03-15 23:40:32 -0400

    remove members from circle

--- backend/controllers/members.ts
+++ backend/controllers/members.ts
@@ -3,6 +3,8 @@ import { getTokenStorage } from "../helpers/tokenStorage.js";
 import SocialCircle from "../models/SocialCircle.js";
 import UserProfile from "../models/UserProfile.js";
 
+// TODO: remove dead code checking if token exists (bc authorize middleware)
+
 /**
  * Get list of members in circle
  */
@@ -33,6 +35,67 @@ const getMembersBySocialCircleId = async (req: Request, res: Response) => {
     }
 };
 
+/**
+ * Remove a member from a circle by memberId
+ * 4 cases: Owner removes self, Owner removes member, Member removes self, Member removes other member
+ */
+function removeMemberFromCircle(req: Request, res: Response) {
+    const { token } = req.cookies;
+    const tokenStorage = getTokenStorage();
+    if (!tokenStorage[token]) {
+        return res.status(401).json({ message: "Invalid token" });
+    }
+
+    const userId = tokenStorage[token].id;
+    const { id, memberId } = req.params;
+    SocialCircle.findById(id)
+        .then((socialCircle) => {
+            if (!socialCircle) {
+                return res
+                    .status(404)
+                    .json({ message: "Social circle not found" });
+            }
+            if (socialCircle.ownerId.toString() != userId) {
+                return res.status(401).json({ message: "Not authorized" });
+            }
+            if (socialCircle.ownerId.toString() == memberId) {
+                if (socialCircle.members.length > 1) {
+                    return res
+                        .status(400)
+                        .json({ message: "Cannot remove owner of circle" });
+                } else {
+                    SocialCircle.findByIdAndDelete(id)
+                        .then(() => {
+                            return res
+                                .status(200)
+                                .json({ message: "Social circle deleted" });
+                        })
+                        .catch((error: any) => {
+                            return res
+                                .status(400)
+                                .json({ message: error.message });
+                        });
+                }
+            } else {
+                SocialCircle.findByIdAndUpdate(id, {
+                    $pull: { members: memberId },
+                })
+                    .then(() => {
+                        return res
+                            .status(200)
+                            .json({ message: "Member removed from circle" });
+                    })
+                    .catch((error: any) => {
+                        return res.status(400).json({ message: error.message });
+                    });
+            }
+        })
+        .catch((error: any) => {
+            return res.status(400).json({ message: error.message });
+        });
+}
+
 export default {
     getMembersBySocialCircleId,
+    removeMemberFromCircle,
 };
--- backend/routes/members.ts
+++ backend/routes/members.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import controller from "../controllers/members.js";
+import { authorize } from "../middleware/checkAuth.js";
 
+// Protected Routes to /api/circles/:id/members
 export const membersRouter: Router = Router({ mergeParams: true });
 
 // TODO: remove passwords from response
-membersRouter.get("/", controller.getMembersBySocialCircleId);
+membersRouter.get("/", authorize, controller.getMembersBySocialCircleId);
+membersRouter.delete("/:memberId", controller.removeMemberFromCircle);
