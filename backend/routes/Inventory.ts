7b3dce2f391670d6beb63abe7910f5e6941b1970
Abe Jeyapratap <abejeyapratap@gmail.com>
2023-03-17 01:51:57 -0400

    inventory use authorize middleware

--- backend/routes/Inventory.ts
+++ backend/routes/Inventory.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
 import controller from "../controllers/inventory.js";
+import { authorize } from "../middleware/checkAuth.js";
 
 export const inventoryRouter: Router = Router();
 
-inventoryRouter.put("/pantry", controller.updatePantry);
-inventoryRouter.get("/pantry", controller.getPantry);
-inventoryRouter.get("/favorite", controller.getFavoriteRecipes);
-inventoryRouter.put("/favorite/:id", controller.addRecipeToFavorite);
-inventoryRouter.delete("/favorite/:id", controller.removeRecipeFromFavorite);
+inventoryRouter.put("/pantry", authorize, controller.updatePantry);
+inventoryRouter.get("/pantry", authorize, controller.getPantry);
+inventoryRouter.get("/favorite", authorize, controller.getFavoriteRecipes);
+inventoryRouter.put("/favorite/:id", authorize, controller.addRecipeToFavorite);
+inventoryRouter.delete(
+    "/favorite/:id",
+    authorize,
+    controller.removeRecipeFromFavorite
+);
 inventoryRouter.get("/myrecipes", controller.getMyRecipes);
