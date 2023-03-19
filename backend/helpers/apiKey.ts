df2fa441d49fa5839a5857a90f094913523ad705
thooper159 <47039665+thooper159@users.noreply.github.com>
2023-03-19 19:30:33 -0400

    display # api keys on open

--- backend/helpers/apiKey.ts
+++ backend/helpers/apiKey.ts
@@ -1,5 +1,6 @@
 let currentKey = 0;
 const apiKeys = process.env.API_KEYS!.split(",");
+console.log("API keys: " + apiKeys);
 
 export function getApiKey() {
     // console.log("Using API key: " + apiKeys[currentKey]);
