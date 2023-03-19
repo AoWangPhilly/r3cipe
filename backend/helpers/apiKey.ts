6a6b92b101e5892131b0e22adbb62bbfb6921fda
thooper159 <47039665+thooper159@users.noreply.github.com>
2023-03-19 19:32:19 -0400

    **

--- backend/helpers/apiKey.ts
+++ backend/helpers/apiKey.ts
@@ -1,6 +1,6 @@
 let currentKey = 0;
 const apiKeys = process.env.API_KEYS!.split(",");
-console.log("API keys: " + apiKeys);
+console.log("API keys: " + apiKeys.length);
 
 export function getApiKey() {
     // console.log("Using API key: " + apiKeys[currentKey]);
